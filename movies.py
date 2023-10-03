95d7c0b88e4a08691e512c98c98395388950129e
Abrahamcepedao <a00827666@itesm.mx>
2023-10-02 19:21:07 -0600

    added more examples

--- movies.py
+++ movies.py
@@ -1,5 +1,5 @@
 '''
-Create a machine learning model with sklearn that uses an external api
+create a model that uses an external api related to movies
 '''
 
 import requests
@@ -9,21 +9,21 @@ from sklearn.model_selection import train_test_split
 
 def main():
     #get the data from the api
-    response = requests.get("https://api.covidtracking.com/v1/us/daily.json")
+    response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=k_mq4vgbc8&language=en-US&page=1")
     data = response.json()
-    print(data)
+    print("data", data)
     #create a dataframe from the data
-    df = pd.DataFrame(data)
+    '''df = pd.DataFrame(data)
     #create a new dataframe with only the columns we want
-    df = df[["positiveIncrease", "deathIncrease"]]
+    df = df[["id", "title", "popularity"]]
     #create a new column that is the difference between positiveIncrease and deathIncrease
-    df["difference"] = df["positiveIncrease"] - df["deathIncrease"]
+    df["difference"] = df["id"] - df["popularity"]
     #create the model
     model = LinearRegression()
     print("model created")
     #split the data into training and testing data
     print("splitting data")
-    X_train, X_test, y_train, y_test = train_test_split(df[["positiveIncrease", "deathIncrease"]], df["difference"], test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(df[["id", "popularity"]], df["difference"], test_size=0.2)
     print("data split")
     #train the model
     model.fit(X_train, y_train)
@@ -32,8 +32,10 @@ def main():
     print(model.score(X_test, y_test))
     print("score printed")
     #predict the difference for a given positiveIncrease and deathIncrease
-    print(model.predict([[100000, 1000]]))
+    print(model.predict([[100000, 1000]]))'''
 
 
 if __name__ == "__main__":
     main()
+
+
